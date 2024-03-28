445411947bcbabe651e757196862ea4236f87258
som <dndvud123@naver.com>
2024-03-28 11:26:45 +0900

    변경 x

--- src/main/java/com/example/FP/controller/InquiryController.java
+++ src/main/java/com/example/FP/controller/InquiryController.java
@@ -1,9 +1,9 @@
 package com.example.FP.controller;
 
 import com.example.FP.entity.InquiryState;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class InquiryController {
-    public static void main(String[] args) {
 
-    }
 }
