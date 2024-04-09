a7d85cb957050719989d1dfb0bfdcdaa8e4db75b
som <dndvud123@naver.com>
2024-04-09 17:52:13 +0900

    kakao login 시도중

--- src/main/java/com/example/FP/config/SecurityConfig.java
+++ src/main/java/com/example/FP/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .requestMatchers("/", "/findUserPwd", "/join","/joinOk","/static/**","/id_check","/sendEmail","/nickname_check","/mailCheck", "/emailAuthentication", "/newPwd", "/findPwdOk", "/findUserid")
+                .requestMatchers("/", "/findUserPwd", "/join","/joinOk","/static/**","/id_check","/sendEmail","/nickname_check","/mailCheck", "/emailAuthentication", "/newPwd", "/findPwdOk", "/findUserid", "/kakaoLogin")
                 .permitAll()
                 .requestMatchers("/admin/**").hasRole("admin")
                 .anyRequest().authenticated()
