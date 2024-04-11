957825df30a78305c64bdb90f5b7b94e9748ced0
YuuuuSeong-Kim <kys980611@gmail.com>
2024-04-11 18:05:33 +0900

    addRecipe 완성 아마도 [김유성]

--- src/main/java/com/example/FP/repository/RecipeRepository.java
+++ src/main/java/com/example/FP/repository/RecipeRepository.java
@@ -5,6 +5,7 @@ import com.example.FP.entity.Recipe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,6 +13,9 @@ public interface RecipeRepository extends JpaRepository<Recipe,Long> {
     Page<Recipe> findPageBy(Pageable page);
 
     List<Recipe> findTop5ByOrderByRecipeViewsDesc();
+
+    @Query(value = "select IFNULL(max(recipe_id),0)+1 from recipe", nativeQuery = true)
+    Long nextRecipeId();
 
     // 가장 조회수가 높은 회원 그룹
 //    List<Recipe> findByRecipeMemberIdGroupByRecipeMemberIdOrderByRecipeViewsDesc();
