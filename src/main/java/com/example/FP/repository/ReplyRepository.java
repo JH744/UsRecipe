60b64b01f731ff7aa526a9dad55783aaabaedfbe
YuuuuSeong-Kim <kys980611@gmail.com>
2024-04-12 17:47:45 +0900

    detailRecipe 제작중 [김유성]

--- src/main/java/com/example/FP/repository/ReplyRepository.java
+++ src/main/java/com/example/FP/repository/ReplyRepository.java
@@ -2,6 +2,10 @@ package com.example.FP.repository;
 
 import com.example.FP.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReplyRepository extends JpaRepository<Reply,Long> {
+
+    @Query(value = "select IFNULL(max(reply_id),0)+1 from reply", nativeQuery = true)
+    Long nextReplyId();
 }
