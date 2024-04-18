a966fc6a91c59a6d92133a113229afc2f3cfdc1b
som <dndvud123@naver.com>
2024-04-18 12:10:14 +0900

    헤더 로그인시 사진,맴버 컬럼 추가

--- src/main/java/com/example/FP/controller/RecipeController.java
+++ src/main/java/com/example/FP/controller/RecipeController.java
@@ -116,7 +116,7 @@ public class RecipeController {
         //찜목록 top4를 불러옴
         List<Recipe> listTop4 =rs.listTop4();
         model.addAttribute("listTop4", listTop4);
-        return "recipe";
+        return "/all/recipe";
     }
 
     //레시피 등록 화면으로 이동
